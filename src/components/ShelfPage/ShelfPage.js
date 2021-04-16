8d431ad723d57003ec5c51b125a50a6dd3135939
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-04-16 09:34:47 -0500

    added html for inputs

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ShelfPage() {
+  const [ objectToSend, setObjectToSend] = useState({});
   return (
     <div className="container">
       <h2>Shelf</h2>
+      <p>Add an Item to the Shelf</p>
+      <label>Description</label>
+      <input type="text"></input>
+      <label>Image Url</label>
+      <input type="text"></input>
       <p>All of the available items can be seen here.</p>
     </div>
   );
